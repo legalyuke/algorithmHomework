b6d62a7eb0e9f2101f6fccfba4f24ad2bf3e352f
liyuke <liyuke@lrc.cn>
2021-06-20 16:55:54 +0800

    第一周作业---设计循环双端队列 增加双向链表实现

--- week01/code/MyCircularDeque.java
+++ week01/code/MyCircularDeque.java
@@ -14,109 +14,261 @@ package code;
  * isEmpty()：检查双端队列是否为空。
  * isFull()：检查双端队列是否满了。
  * 实现方法1：
+ * 使用双向链表实现
+ * 实现方法2：
  * 使用数组实现
+ *
  * @author liyuke
  * @date 2021-06-20 11:58
  */
 public class MyCircularDeque {
 
-    int size;
-    int used;
-    int[] value;
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode pre;
 
-    /**
-     * Initialize your data structure here. Set the size of the deque to be k.
-     */
-    public MyCircularDeque(int k) {
-        size = k;
-        value = new int[size];
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode pre, ListNode next) {
+            this.val = val;
+            this.pre = pre;
+            this.next = next;
+        }
     }
 
-    /**
-     * Adds an item at the front of Deque. Return true if the operation is successful.
-     */
-    public boolean insertFront(int value) {
-        if (isFull()) {
-            return false;
+    //方法1 用双向链表实现
+    class LinkedListImpl {
+
+        int size;
+        int used;
+        ListNode head;
+        ListNode tail;
+
+        /**
+         * Initialize your data structure here. Set the size of the deque to be k.
+         */
+        public LinkedListImpl(int k) {
+            size = k;
         }
-        for (int i = used; i > 0; i--) {
-            this.value[i] = this.value[i - 1];
+
+        /**
+         * Adds an item at the front of Deque. Return true if the operation is successful.
+         */
+        public boolean insertFront(int value) {
+            if (isFull()) {
+                return false;
+            }
+            ListNode cur = new ListNode(value);
+            if (isEmpty()) {
+                head = cur;
+                tail = cur;
+            } else {
+                head.pre = cur;
+                cur.next = head;
+                head = cur;
+            }
+            used++;
+            return true;
         }
-        this.value[0] = value;
-        used++;
-        return true;
+
+        /**
+         * Adds an item at the rear of Deque. Return true if the operation is successful.
+         */
+        public boolean insertLast(int value) {
+            if (isFull()) {
+                return false;
+            }
+            ListNode cur = new ListNode(value);
+            if (isEmpty()) {
+                head = cur;
+                tail = cur;
+            } else {
+                cur.pre = tail;
+                tail.next = cur;
+                tail = cur;
+            }
+            used++;
+            return true;
+        }
+
+        /**
+         * Deletes an item from the front of Deque. Return true if the operation is successful.
+         */
+        public boolean deleteFront() {
+            if (isEmpty()) {
+                return false;
+            }
+            if (head == tail) {
+                head = null;
+                tail = null;
+            } else {
+                head = head.next;
+                head.pre = null;
+            }
+            used--;
+            return true;
+        }
+
+        /**
+         * Deletes an item from the rear of Deque. Return true if the operation is successful.
+         */
+        public boolean deleteLast() {
+            if (isEmpty()) {
+                return false;
+            }
+            if (head == tail) {
+                head = null;
+                tail = null;
+            } else {
+                tail = tail.pre;
+                tail.next = null;
+            }
+            used--;
+            return true;
+        }
+
+        /**
+         * Get the front item from the deque.
+         */
+        public int getFront() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return head.val;
+        }
+
+        /**
+         * Get the last item from the deque.
+         */
+        public int getRear() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return tail.val;
+        }
+
+        /**
+         * Checks whether the circular deque is empty or not.
+         */
+        public boolean isEmpty() {
+            return used == 0;
+        }
+
+        /**
+         * Checks whether the circular deque is full or not.
+         */
+        public boolean isFull() {
+            return used == size;
+        }
     }
 
-    /**
-     * Adds an item at the rear of Deque. Return true if the operation is successful.
-     */
-    public boolean insertLast(int value) {
-        if (isFull()) {
-            return false;
-        }
-        this.value[used] = value;
-        used++;
-        return true;
-    }
+    //方法2 用数组实现
+    class ArrayImpl {
 
-    /**
-     * Deletes an item from the front of Deque. Return true if the operation is successful.
-     */
-    public boolean deleteFront() {
-        if (isEmpty()) {
-            return false;
-        }
-        for (int i = 0; i < used - 1; i++) {
-            value[i] = value[i + 1];
-        }
-        value[used - 1] = 0;
-        used--;
-        return true;
-    }
+        int size;
+        int used;
+        int[] value;
 
-    /**
-     * Deletes an item from the rear of Deque. Return true if the operation is successful.
-     */
-    public boolean deleteLast() {
-        if (isEmpty()) {
-            return false;
+        /**
+         * Initialize your data structure here. Set the size of the deque to be k.
+         */
+        public ArrayImpl(int k) {
+            size = k;
+            value = new int[size];
         }
-        value[used - 1] = 0;
-        used--;
-        return true;
-    }
 
-    /**
-     * Get the front item from the deque.
-     */
-    public int getFront() {
-        if (isEmpty()) {
-            return -1;
+        /**
+         * Adds an item at the front of Deque. Return true if the operation is successful.
+         */
+        public boolean insertFront(int value) {
+            if (isFull()) {
+                return false;
+            }
+            for (int i = used; i > 0; i--) {
+                this.value[i] = this.value[i - 1];
+            }
+            this.value[0] = value;
+            used++;
+            return true;
         }
-        return value[0];
-    }
 
-    /**
-     * Get the last item from the deque.
-     */
-    public int getRear() {
-        if (isEmpty()) {
-            return -1;
+        /**
+         * Adds an item at the rear of Deque. Return true if the operation is successful.
+         */
+        public boolean insertLast(int value) {
+            if (isFull()) {
+                return false;
+            }
+            this.value[used] = value;
+            used++;
+            return true;
         }
-        return value[used - 1];
-    }
 
-    /**
-     * Checks whether the circular deque is empty or not.
-     */
-    public boolean isEmpty() {
-        return used == 0;
-    }
+        /**
+         * Deletes an item from the front of Deque. Return true if the operation is successful.
+         */
+        public boolean deleteFront() {
+            if (isEmpty()) {
+                return false;
+            }
+            for (int i = 0; i < used - 1; i++) {
+                value[i] = value[i + 1];
+            }
+            value[used - 1] = 0;
+            used--;
+            return true;
+        }
 
-    /**
-     * Checks whether the circular deque is full or not.
-     */
-    public boolean isFull() {
-        return used == size;
+        /**
+         * Deletes an item from the rear of Deque. Return true if the operation is successful.
+         */
+        public boolean deleteLast() {
+            if (isEmpty()) {
+                return false;
+            }
+            value[used - 1] = 0;
+            used--;
+            return true;
+        }
+
+        /**
+         * Get the front item from the deque.
+         */
+        public int getFront() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return value[0];
+        }
+
+        /**
+         * Get the last item from the deque.
+         */
+        public int getRear() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return value[used - 1];
+        }
+
+        /**
+         * Checks whether the circular deque is empty or not.
+         */
+        public boolean isEmpty() {
+            return used == 0;
+        }
+
+        /**
+         * Checks whether the circular deque is full or not.
+         */
+        public boolean isFull() {
+            return used == size;
+        }
     }
 }
